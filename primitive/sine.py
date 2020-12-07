605964e8541cc49ce3e61105b98933a056ff2c9d
kickscar <kickscar@gmail.com>
2020-12-08 06:01:56 +0900

    Initial commit

---
+++ primitive/sine.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def sine(i):
+    sinegraph.set_data(X[:i], Y[:i])
+    dot.set_data(X[i], Y[i])
+
+
+X = np.linspace(0, 2*np.pi, 100)
+Y = np.sin(X)
+
+fig, ax = plt.subplots(1,1)
+ax.set_xlim([0, 2*np.pi])
+ax.set_ylim([-1.1, 1.1])
+
+sinegraph, = ax.plot([], [])
+dot, = ax.plot([], [], 'o', color='red')
+
+
+anim = animation.FuncAnimation(fig, sine, frames=len(X), interval=50)
+
+plt.grid()
+plt.show()
